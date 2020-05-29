a46c4e57733d983253b82841b957fb2693afff7a
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-05-29 11:04:57 +0200

    adding __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pmmpc',
+    version='0.0.1',
+    url='https://github.com/zanellia/ptmpc.git',
+    author='Andrea Zanelli',
+    author_email='znllandrea@gmail.com',
+    description='Python implementation of partially tightened MPC',
+    packages=find_packages(),    
+    install_requires=['numpy', 'casadi'],
+)
